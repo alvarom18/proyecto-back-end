10a9e4ad25b8d1124afbcdff003d561d62f2e450
Alvaro Mora <alyua76@gmail.com>
2023-07-14 00:01:45 -0400

    practica integradora

--- ProductManager.js
+++ ProductManager.js
@@ -37,3 +37,41 @@ app.use("/api/cart", cartRouter);
 app.listen(port, () => {
   console.log(`servidor ${port}`);
 });
+
+// Importar mongoose
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb://<user>:<clave>@<host>:<port>/ecommerce", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const cartSchema = new mongoose.Schema({
+  user: { type: String, required: true },
+  products: [{ type: mongoose.Schema.Types.ObjectId, ref: "Product" }],
+});
+
+const messageSchema = new mongoose.Schema({
+  sender: { type: String, required: true },
+  text: { type: String, required: true },
+  date: { type: Date, default: Date.now },
+});
+
+const productSchema = new mongoose.Schema({
+  // Atributos del schema
+  name: { type: String, required: true },
+  description: { type: String, required: true },
+  price: { type: Number, required: true },
+  stock: { type: Number, default: 0 },
+  image: { type: String },
+});
+
+const Cart = mongoose.model("Cart", cartSchema);
+const Message = mongoose.model("Message", messageSchema);
+const Product = mongoose.model("Product", productSchema);
+
+module.exports = {
+  Cart,
+  Message,
+  Product,
+};
